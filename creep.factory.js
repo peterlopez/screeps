5ce27a08d6d446fa947fff1a9cc32fc9bc91647d
Peter Lopez <peter@peterlopezweb.com>
2017-01-11 19:56:31 -0800

    Created module for creep production logic

--- creep.factory.js
+++ creep.factory.js
@@ -8,7 +8,7 @@ const CREEP_BODY_PARTS = {
     'harvester' : [WORK, WORK, WORK, CARRY, MOVE],
     'upgrader'  : [WORK, WORK, WORK, CARRY, MOVE],
     'builder'   : [WORK, CARRY, CARRY, MOVE, MOVE],
-    'fixer' 	: [WORK, CARRY, CARRY, MOVE, MOVE],
+    'fixer'     : [WORK, CARRY, CARRY, MOVE, MOVE],
 };
 const CREEPS_QUOTA = {
     'harvester': 4,
@@ -18,17 +18,34 @@ const CREEPS_QUOTA = {
 };
 
 /**
- * 
+ * Determine what kind of creep to produce
+ * based on current creep roles and quotas
  */
 creepFactory.getNewCreepRole = function() {
-	console.log("todo");
+    var newRole;
+    var diff = 0; // descrepency between creep count and quota
+    var creepRoles = require('utils').getCreepsByRole();
+
+    // Loop through each creep role
+    // comparing counts with quotas
+    for(role in creepRoles) {
+        var currentDiff = CREEPS_QUOTA[role] - creepRoles[role].length;
+
+        newRole = currentDiff > diff ? role : newRole;
+        diff = currentDiff > diff ? currentDiff : diff;
+    }
+
+    if(diff == 0) {
+        return false;
+    }
+    return newRole;
 };
 
 /**
- *
+ * Spawns creep with given body parts based on role
  */
 creepFactory.createCreep = function(role) {
-	console.log("todo");
+    return Game.spawns['Spawn1'].createCreep(CREEP_BODY_PARTS[role], undefined, {role: role});
 }
 
 module.exports = creepFactory;
--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-// https://raw.githubusercontent.com/screeps/tutorial-scripts/master/section5/main.js
-
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleFixer = require('role.fixer');
-
-const CREEP_BODY_PARTS = {
-    'harvester' : [WORK, WORK, WORK, CARRY, MOVE],
-    'upgrader'  : [WORK, WORK, WORK, CARRY, MOVE],
-    'builder'   : [WORK, CARRY, CARRY, MOVE, MOVE],
-    'fixer'     : [WORK, CARRY, CARRY, MOVE, MOVE],
-};
-const MAX_CREEPS = {
-    'harvester': 4,
-    'upgrader': -1,
-    'builder': 2,
-    'fixer': 2,
-};
 
 module.exports.loop = function () {
     // Clear decesed creeps from memory
@@ -38,35 +23,14 @@ module.exports.loop = function () {
             extensionsFull = false;
         }
     }
-    if(Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity && extensionsFull) {
-        
-        // Get total amount of each creep
-        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-        var fixer = _.filter(Game.creeps, (creep) => creep.memory.role == 'fixer');
     
-        // Find creep type with least amount of creeps
-        var allCreeps = {'harvester': harvesters, 'builder': builders, 'upgrader': upgraders, 'fixer': fixer};
-        var leastCreeps = allCreeps.upgrader.length;
-        var leastCreepsType = 'upgrader';
-
-        for(var type in allCreeps) {
-            var numCreepsOfType = allCreeps[type].length;
-
-            // Skip creep type if already at max
-            if( numCreepsOfType >= MAX_CREEPS[type] && MAX_CREEPS[type] !== -1 ) {
-                continue;
-            }
-            if( allCreeps[type].length < leastCreeps ) {
-                leastCreeps = allCreeps[type].length;
-                leastCreepsType = type;
-            }
+    if(Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity && extensionsFull) {
+        var creepFactory = require('creep.factory');
+        var newCreepRole = creepFactory.getNewRole();
+        if(newCreepRole) {
+            console.log("Spawning new '"+newCreepRole+"'");
+            creepFactory.createCreep(newCreepRole);
         }
-        console.log("'Least creeps are '"+leastCreepsType+"' with only: "+leastCreeps+" creeps");
-        console.log("Spawn1 at capacity, building: '"+leastCreepsType+"'");
-        
-        Game.spawns['Spawn1'].createCreep(CREEP_BODY_PARTS[leastCreepsType], undefined, {role: leastCreepsType});
     }
     
     var tower = Game.getObjectById('TOWER_ID');
